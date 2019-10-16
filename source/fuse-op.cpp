76ca779f50983b4f02c542161b79779211c87112
Gabriele <sucre.91@hotmail.it>
2019-10-16 15:58:21 +0200

    Defining rucio-fuse-lib and cleaning up

--- source/fuse-op.cpp
+++ source/fuse-op.cpp
@@ -1,15 +1,3 @@
 //
 // Created by Gabriele Gaetano Fronzé on 2019-10-16.
 //
-
-//#include <fuse-op.h>
-//#include <REST-API.h>
-
-//static int rucio_readdir (const char *path, void *buff, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi){
-//  printf("rucio_readdir called");
-//  return 0;
-//}
-//static int rucio_read (const char *path, char *buff, size_t size, off_t offset, struct fuse_file_info *fi){
-//  printf("rucio_read called");
-//  return 0;
-//}
